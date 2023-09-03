c6374af8cb6d84f3f27efae3628fd1d4d1bb226c
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-09-03 16:42:16 -0700

    added connection to the firebase data

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import * as firebase from 'firebase';
+import '@firebase/auth';
+import '@firebase/firestore';
+
 import { StyleSheet, View } from 'react-native';
 
 import WeeklyCalendar  from './components/Calendar/WeeklyCalendar';
@@ -31,6 +35,30 @@ import { store } from './store/redux/store';
 
 const Stack = createNativeStackNavigator();
 const BottomTab = createBottomTabNavigator();
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAUuZUgvlmMYBOl_Kg43Eb2sHnEJp4_us4",
+
+  authDomain: "sandler-cookbook.firebaseapp.com",
+
+  projectId: "sandler-cookbook",
+
+  storageBucket: "sandler-cookbook.appspot.com",
+
+  messagingSenderId: "519878306197",
+
+  appId: "1:519878306197:web:4fe836d84e5caae16dae9c"
+
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+
+firebase.initializeApp(config);
+
 
 function AuthStack() {
   return (
